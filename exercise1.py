d991fdaaea824e7ff5ec11bb52cf5bd41120f4f5
michaelmk708 <michaelkinuthia708@gmail.com>
2026-05-27 11:03:43 +0300

    v1.0

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+user_data = {
+    "user_id": 101,
+    "name":"mike",
+    "email":"michaelkinuthia708@gmail.com",
+    "age":20,
+    "expenses": [
+        {"id": 1, "amount": 100, "category": "food"},
+        {"id": 2, "amount": 50, "category": "transport"},
+        {"id": 3, "amount": 200, "category": "entertainment"}
+    ]
+}
---
+++ script.py
@@ -0,0 +1,11 @@
+from exercise1 import *
+
+print(user_data["expenses"][1]["amount"])
+
+def create_expense(amount, **kwargs):
+    expense = {"amount":amount}
+    expense.update(kwargs)
+    return expense
+
+final_data= create_expense(150, id=4, category="utilities")
+print(final_data)
